96627865418113e1ebeff16da21d6135bba3a699
Aybars Badur <abadur@consultant.ffn.com>
2023-07-21 18:45:44 +0200

    replace image

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup, find_packages
 
-# Read the dependencies from requirements.txt
-with open('requirements.txt', 'r') as file:
-    install_requires = file.read().splitlines()
+install_requires = """
+pdfplumber==0.10.1
+langchain==0.0.238
+openai==0.27.8
+pdfplumber==0.10.1
+pdfminer==20191125
+click==8.1.6
+openpyxl==3.1.2
+""".strip().splitlines()
 
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
     name='InvoiceExtractor',
-    version='0.1.0',
+    version='0.1.2',
     author='Aybars Badur',
+    include_package_data=True,
     description='A Python package for extracting data from invoices.',
     long_description=long_description,
     long_description_content_type='text/markdown',
